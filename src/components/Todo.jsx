e6b6f264117ac6e1cc658629ee3b5869425839c3
alejandro <alejo4373@gmail.com>
2019-04-16 21:41:34 -0400

    remove todo button hidden until hover

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -4,17 +4,15 @@ const Todo = (props) => {
   const { todo, deleteTodo, toggleCompleted } = props
   return (
     <li>
-      <div>
-        <span
-          onClick={toggleCompleted}
-          data-todo_id={todo.id}
-          className={todo.completed ? "completed" : null}
-        >
-          <input type="checkbox" readOnly checked={todo.completed} />
-          {todo.text}
-        </span>
-        <button id={todo.id} onClick={deleteTodo}>X</button>
-      </div>
+      <span
+        onClick={toggleCompleted}
+        data-todo_id={todo.id}
+        className={todo.completed ? "completed" : null}
+      >
+        <input type="checkbox" readOnly checked={todo.completed} />
+        {todo.text}
+      </span>
+      <button className="btn_remove" id={todo.id} onClick={deleteTodo}>X</button>
     </li>
   )
 }
