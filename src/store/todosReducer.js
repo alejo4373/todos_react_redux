8347a4200e5bf1d5564247ea1a96aa62b8391145
Alejo <alejandrofranco@pursuit.org>
2020-01-24 23:51:17 -0500

    remove break after return in todosReducer addressing unreachable code react warning

--- src/store/todosReducer.js
+++ src/store/todosReducer.js
@@ -1,24 +1,22 @@
-import { RECEIVE_TODO, DELETE_TODO, TOGGLE_COMPLETED }  from './actionTypes';
+import { RECEIVE_TODO, DELETE_TODO, TOGGLE_COMPLETED } from './actionTypes';
 const todosReducer = (state = {}, { type, todo }) => {
-  const newState = {...state};
+  const newState = { ...state };
   switch (type) {
     case RECEIVE_TODO:
       newState[todo.id] = todo;
       return newState;
-      break;
 
-   case DELETE_TODO:
+    case DELETE_TODO:
       delete newState[todo.id];
       return newState;
-      break; 
 
-   case TOGGLE_COMPLETED:
+    case TOGGLE_COMPLETED:
       newState[todo.id].completed = !newState[todo.id].completed;
       return newState;
-      
+
     default:
       return state;
-  } 
+  }
 }
 
 export default todosReducer;
