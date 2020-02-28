23b46a11dc705529046518307e50a6a2d0e2316f
Alejo <alejandrofranco@pursuit.org>
2020-02-27 22:03:48 -0500

    adjust network requests url to address server remoing verb from URIs

--- src/api.js
+++ src/api.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-export const addTodo = (todo) => axios.post('/api/todos/new', todo)
-export const fetchTodos = () => axios.get('/api/todos/all')
+export const addTodo = (todo) => axios.post('/api/todos', todo)
+export const fetchTodos = () => axios.get('/api/todos')
 export const updateTodo = (id, updates) => axios.patch(`/api/todos/${id}`, updates)
 export const deleteTodo = (id) => axios.delete(`/api/todos/${id}`)
 
-export const addJournalEntry = (entry) => axios.post('/api/journal/add', entry)
+export const addJournalEntry = (entry) => axios.post('/api/journal/entries', entry)
 export const fetchJournalEntries = () => axios.get('/api/journal/entries')
 
 export const login = (credentials) => axios.post('/api/auth/login', credentials)
