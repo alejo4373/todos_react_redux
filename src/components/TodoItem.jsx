3fd175aa68280f1d6a24047cc6cc697114568052
alejo4373 <alejandrofranco@pursuit.org>
2020-04-23 02:49:19 -0400

    complete todo by clicking checkbox an not by clicking whole todo text since it is now a link

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -5,12 +5,17 @@ const TodoItem = (props) => {
   const { todo, deleteTodo, toggleCompleted } = props
   return (
     <li className="todo-item">
+      <input
+        type="checkbox"
+        checked={todo.completed}
+        onChange={toggleCompleted}
+        data-todo_id={todo.id}
+      />
       <Link
         to={`/todos/${todo.id}`}
         data-todo_id={todo.id}
         className={'todo-content ' + (todo.completed ? "completed" : "")}
       >
-        <input type="checkbox" readOnly checked={todo.completed} />
         {todo.text}
       </Link>
       <button className="btn_remove" id={todo.id} onClick={deleteTodo}>X</button>
