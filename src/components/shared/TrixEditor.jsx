31a354e58d0408bceb893d536a8b8c64e51e86e8
alejo4373 <alejandrofranco@pursuit.org>
2021-05-04 01:16:49 -0400

    Clear TrixEditor when value.length < 1

--- src/components/shared/TrixEditor.jsx
+++ src/components/shared/TrixEditor.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import {
   ReactTrixRTEInput as TrixEditor,
   ReactTrixRTEToolbar as TrixToolbar
@@ -8,10 +8,19 @@ import "../../styles/TrixEditor.css"
 
 const Editor = ({ value, onChange, placeholder, id }) => {
   let options = ["heading1", "bold", "italic", "link", "bullet", "number", "undo", "redo"]
+  let editorRef = useRef(null)
 
   const handleChange = (e, value) => {
     onChange(value)
   }
+
+  useEffect(() => {
+    const { editor } = editorRef.current
+    if (value.length < 1 && editor.getDocument().toString().length > 1) {
+      editor.loadJSON({ document: [] })
+    }
+  }, [value])
+
   return (
     <div className="editor" >
       <TrixToolbar
@@ -23,6 +32,7 @@ const Editor = ({ value, onChange, placeholder, id }) => {
         defaultValue={value}
         placeholder={placeholder}
         onChange={handleChange}
+        trixInputRef={editorRef}
         toolbarId={`trix-toolbar${id ? '-' + id : ''}`}
       />
     </div>
